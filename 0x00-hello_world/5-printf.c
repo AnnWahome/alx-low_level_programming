f05847d375d40e7c80569fd68f5918a46cc260ea
AnnWahome <annsonnie1@gmail.com>
2022-03-15 00:14:53 +0300

    Hello world task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Printf task 5
+*Return: return 0 after execution
+*/
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
+
