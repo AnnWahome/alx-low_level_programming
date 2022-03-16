ad1a370005905662c55895b0005b4d9e533f57b4
AnnWahome <annsonnie1@gmail.com>
2022-03-16 05:58:16 +0300

    Print first 50 fibonacci numbers, start with 1 and 2

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 /**
  * main -  prints the first 50 Fibonacci numbers, starting with 1 and 2
- * no argument
+ * @void: no argument
  * Return: 0
  */
 int main(void)
 {
-	long fir = 1, sec = 2, sum;
+	long pri = 1, sec = 2, sum;
 
 	printf("1, 2");
 	while (sum < 20365011073)
 	{
-		sum = fri + sec;
+		sum = pri + sec;
 		printf(", %lu", sum);
-		fri = sec;
+		pri = sec;
 		sec = sum;
 	}
 	printf("\n");
