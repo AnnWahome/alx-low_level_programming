924e92352055e7dc890b5e25ced5b44c0a91cbb6
AnnWahome <annsonnie1@gmail.com>
2022-03-18 00:49:37 +0300

    Checks uppercase and prints 1

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _isupper(int c)
+
+/*
+ *File:main.h
+ *Desc: Header file containing all prototypes used in 0x03-more_
+ * functions_nested_loops directory
+ *
+ */
+
+int _isupper(int c);
+
+#endif
