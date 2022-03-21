2f9fd73188e4d5dba99ec2bd4850e89cd54fe573
AnnWahome <annsonnie1@gmail.com>
2022-03-22 02:04:54 +0300

    Print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_puts - prints string to standard output
+ *@str: Character Pointer input
+ *Returns: Nothing
+ */
+void _puts(char *str)
+{
+  int j;
+  j=0;
+
+  while(str[j] != '\0')
+    {
+     _putchar(str[j]);
+     j++;
+    }
+  _putchar('\n');
+}
